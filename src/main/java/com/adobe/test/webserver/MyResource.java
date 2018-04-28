92f7c870f9056ee0bb55f77167a6ee03fc14968b
sanadhis <sanadhis@gmail.com>
2018-04-28 15:15:34 +0200

    --Change to Jersey and Grizzly

---
+++ src/main/java/com/adobe/test/webserver/MyResource.java
@@ -0,0 +1,45 @@
+package com.adobe.test.webserver;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+    public static final String projectPath = System.getProperty("user.dir");
+    public static final String basePath = projectPath + "/resources";
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/html" media type.
+     *
+     * @return InputStream that will be returned as a text/html response.
+     */
+    @GET
+    @Path("{filename}")
+    @Produces(MediaType.TEXT_HTML)
+    public InputStream getResponse(@PathParam("filename") String filename) {
+        System.out.println(filename);
+        try{
+            File f = new File(basePath + "/" + filename);
+            return new FileInputStream(f);
+        }
+        catch(FileNotFoundException e){
+            String response = "<body><h1>File not found</h1></body>";
+            InputStream stream  = new ByteArrayInputStream(response.getBytes(StandardCharsets.UTF_8));
+            return stream;
+        }
+    }
+}
